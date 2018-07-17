7469a521c8ca98f2b74d8fcdab242a2d12badfa1
heflat <h@heflat.com>
2018-07-17 16:49:43 +0800

    Update JsonUtil

--- src/main/java/com/heflat/utils/util/JsonUtil.java
+++ src/main/java/com/heflat/utils/util/JsonUtil.java
@@ -3,6 +3,9 @@ package com.heflat.utils.util;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.lang.reflect.Type;
+import java.util.List;
+
 /**
  * Json 工具类
  *
@@ -12,9 +15,9 @@ import com.google.gson.GsonBuilder;
 public class JsonUtil {
 
     /**
-     * Object 转 Json 格式
+     * Object 转 Json
      *
-     * @param object
+     * @param object Object 对象
      * @return
      */
     public static String toJson(Object object) {
@@ -23,10 +26,10 @@ public class JsonUtil {
     }
 
     /**
-     * Object 转 Json 格式
+     * Object 转 Json
      * 格式化输出
      *
-     * @param object
+     * @param object Object 对象
      * @return
      */
     public static String toJsonPrint(Object object) {
@@ -37,14 +40,26 @@ public class JsonUtil {
     }
 
     /**
-     * Json 格式转 Object
+     * Json 转 Object
      *
-     * @param json
-     * @param clazz
+     * @param json Json 字符串
+     * @param clazz Object.class
      * @return
      */
     public static Object toObject(String json, Class clazz) {
         Gson gson = new Gson();
         return gson.fromJson(json, clazz);
+    }
+
+    /**
+     * Json 转 List<T>
+     *
+     * @param json json 字符串
+     * @param type new TypeToken<List<T>>() {}.getType()
+     * @return
+     */
+    public static <T> List<T> toList(String json, Type type) {
+        Gson gson = new Gson();
+        return gson.fromJson(json, type);
     }
 }
